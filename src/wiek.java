422ff167328b6478ff18ca22a66ae6a756af65c6
Kinga Banasik <kingab40@gmail.com>
2021-08-06 22:13:38 +0200

    Lesson 1

---
+++ src/wiek.java
@@ -0,0 +1,12 @@
+public class wiek {
+    public static void main(String[] args) {
+        int wiek = 30;
+
+        if (wiek <= 30){
+            System.out.println("Czlowiek jest mlody");
+        } else if ( wiek > 30){
+            System.out.println("Czlowiek jest sredniego wieku lub stary");
+
+        }
+    }
+}
